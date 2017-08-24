7ce10a18e38cc2b39f4a08724f105e3dd15797d7
petersommerhoff@gmail.com <petersommerhoff@gmail.com>
2017-08-24 22:04:05 +0200

    Initial commit.

---
+++ src/basics/collections-loops.kt
@@ -0,0 +1,29 @@
+package basics
+
+import java.util.*
+
+/*
+ * Challenge: Using collections and loops in Kotlin
+ *
+ * Kotlin clearly differentiates between mutable and immutable collections. Also, the "for" loop is
+ * similar to the for-in (or for-each) loop in Java. "while" loops follow the same syntax as in Java.
+ * In this challenge, you'll apply what you learned about collections and loops.
+ *
+ * This challenge is part of the course "Kotlin for Android and Java Developers": [TODO: add link]
+ *
+ * @author Peter Sommerhoff
+ */
+fun main(args: Array<String>) {
+
+    val randoms: MutableList<Int> = mutableListOf()  // you can also use mutableListOf<Int>()
+
+    for (i in 1..100) {
+        randoms.add(Random().nextInt(100) + 1)
+    }
+
+    var i = 0
+    while(randoms[i] > 10) {
+        println(randoms[i])
+        i++
+    }
+}
---
+++ src/basics/if-expression.kt
@@ -0,0 +1,35 @@
+package basics
+
+/*
+ * Challenge: Using if-expressions in Kotlin
+ *
+ * In Kotlin, you can use "if" as an expression with a value, not just as a statement as in Java.
+ * This challenge familiarizes you with the use of "if" as an expression.
+ *
+ * This challenge is part of the course "Kotlin for Android and Java Developers": [TODO: add link]
+ *
+ * @author Peter Sommerhoff
+ */
+fun main(args: Array<String>) {
+
+    val anonymous = "Anonymous"
+
+    print("Enter your name: ")
+    val input = readLine()
+
+    // Use anonymous name if none is given
+    val name = if (input != null && input.isNotBlank()) {
+        input
+    } else {
+        anonymous
+    }
+
+    // Another if-expression
+    val message = if (name == anonymous) {
+        "Conscious about your privacy, eh?"
+    } else {
+        "Welcome $name, the CIA has been informed of your name :)"
+    }
+
+    println(message)  // You could also directly use println() above
+}
