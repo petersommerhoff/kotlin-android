2796b1d3909367bb2e6f5ddc06a9a22154d6e7e8
petersommerhoff@gmail.com <petersommerhoff@gmail.com>
2018-04-02 08:21:58 +0200

    Fixed while condition

--- src/basics/collections-loops.kt
+++ src/basics/collections-loops.kt
@@ -9,21 +9,22 @@ import java.util.*
  * similar to the for-in (or for-each) loop in Java. "while" loops follow the same syntax as in Java.
  * In this challenge, you'll apply what you learned about collections and loops.
  *
- * This challenge is part of the course "Kotlin for Android and Java Developers": [TODO: add link]
+ * This challenge is part of the course "Kotlin for Android and Java Developers":
+ * https://www.udemy.com/kotlin-android/?couponCode=GITHUB17
  *
  * @author Peter Sommerhoff
  */
 fun main(args: Array<String>) {
 
-    val randoms: MutableList<Int> = mutableListOf()  // you can also use mutableListOf<Int>()
+  val randoms: MutableList<Int> = mutableListOf()  // you can also use mutableListOf<Int>()
 
-    for (i in 1..100) {
-        randoms.add(Random().nextInt(100) + 1)
-    }
+  for (i in 1..100) {
+    randoms.add(Random().nextInt(100) + 1)
+  }
 
-    var i = 0
-    while(randoms[i] > 10) {
-        println(randoms[i])
-        i++
-    }
+  var i = 0
+  while (randoms[i] > 10 && i < randoms.size) {
+    println(randoms[i])
+    i++
+  }
 }
