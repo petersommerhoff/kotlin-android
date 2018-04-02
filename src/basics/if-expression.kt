24b10ef06f137e4768e6fe3d076c965b61db0826
petersommerhoff@gmail.com <petersommerhoff@gmail.com>
2018-04-02 08:22:12 +0200

    Add links to course

--- src/basics/if-expression.kt
+++ src/basics/if-expression.kt
@@ -6,30 +6,31 @@ package basics
  * In Kotlin, you can use "if" as an expression with a value, not just as a statement as in Java.
  * This challenge familiarizes you with the use of "if" as an expression.
  *
- * This challenge is part of the course "Kotlin for Android and Java Developers": [TODO: add link]
+ * This challenge is part of the course "Kotlin for Android and Java Developers":
+ * https://www.udemy.com/kotlin-android/?couponCode=GITHUB17
  *
  * @author Peter Sommerhoff
  */
 fun main(args: Array<String>) {
 
-    val anonymous = "Anonymous"
+  val anonymous = "Anonymous"
 
-    print("Enter your name: ")
-    val input = readLine()
+  print("Enter your name: ")
+  val input = readLine()
 
-    // Use anonymous name if none is given
-    val name = if (input != null && input.isNotBlank()) {
-        input
-    } else {
-        anonymous
-    }
+  // Use anonymous name if none is given
+  val name = if (input != null && input.isNotBlank()) {
+    input
+  } else {
+    anonymous
+  }
 
-    // Another if-expression
-    val message = if (name == anonymous) {
-        "Conscious about your privacy, eh?"
-    } else {
-        "Welcome $name, the CIA has been informed of your name :)"
-    }
+  // Another if-expression
+  val message = if (name == anonymous) {
+    "Conscious about your privacy, eh?"
+  } else {
+    "Welcome $name, the CIA has been informed of your name :)"
+  }
 
-    println(message)  // You could also directly use println() above
+  println(message)  // You could also directly use println() above
 }
--- src/functional/function-chaining.kt
+++ src/functional/function-chaining.kt
@@ -7,36 +7,37 @@ package functional
  * and functional programming concepts to do simple data analysis. Explore the standard library
  * by using IntelliJ's autocompletion to find appropriate functions.
  *
- * This challenge is part of the course "Kotlin for Android and Java Developers": [TODO: add link]
+ * This challenge is part of the course "Kotlin for Android and Java Developers":
+ * https://www.udemy.com/kotlin-android/?couponCode=GITHUB17
  *
  * @author Peter Sommerhoff
  */
 fun main(args: Array<String>) {
 
-    // Some faulty data with ages of our users
-    val data = mapOf(
-            "users1.csv" to listOf(32, 45, 17, -1, 34),
-            "users2.csv" to listOf(19, -1, 67, 22),
-            "users3.csv" to listOf(),
-            "users4.csv" to listOf(56, 32, 18, 44)
-    )
+  // Some faulty data with ages of our users
+  val data = mapOf(
+      "users1.csv" to listOf(32, 45, 17, -1, 34),
+      "users2.csv" to listOf(19, -1, 67, 22),
+      "users3.csv" to listOf(),
+      "users4.csv" to listOf(56, 32, 18, 44)
+  )
 
-    // 1) Average age of users
-    val averageAge = data.flatMap { it.value }
-            .filter { it >= 0 }
-            .average()
+  // 1) Average age of users
+  val averageAge = data.flatMap { it.value }
+      .filter { it >= 0 }
+      .average()
 
-    // 2) Files with faulty data
-    val faultyFiles = data.filter { it.value.any { it < 0 } }
-            .map { it.key }
+  // 2) Files with faulty data
+  val faultyFiles = data.filter { it.value.any { it < 0 } }
+      .map { it.key }
 
-    // 3) Number of faulty entries
-    val numberOfFaults = data.flatMap { it.value }
-            .filter { it < 0 }
-            .count()
+  // 3) Number of faulty entries
+  val numberOfFaults = data.flatMap { it.value }
+      .filter { it < 0 }
+      .count()
 
 
-    println("Users average %.2f years of age.".format(averageAge))
-    println("Files with faulty data: $faultyFiles")
-    println("There were $numberOfFaults errors in the data.")
+  println("Users average %.2f years of age.".format(averageAge))
+  println("Files with faulty data: $faultyFiles")
+  println("There were $numberOfFaults errors in the data.")
 }
